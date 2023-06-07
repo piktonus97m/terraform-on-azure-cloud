e21c03ab109ba2ebfce60af65e7b4e3bad0a292b
Francisco <piktonus97x@hotmail.com>
2023-06-06 19:49:52 -0600

    creating a subnet, public ip and nic

--- 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network-practice.tf
+++ 09-Resource-Syntax-and-Behavior/terraform-manifests/c3-virtual-network-practice.tf
@@ -1,22 +1,40 @@
 # Resource 2: Create a Virtual Networks
 # Reference: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
 
-resource "azurerm_virtual_network" "name" {
-    name = "xorg-consulting-stage-vnet-01"
-    location = azurerm_resource_group.xorg_consulting_stage.location
-    resource_group_name = azurerm_resource_group.xorg_consulting_stage.name
-    address_space = [ "10.0.0.0/16" ]
-    tags = {
-      "Name" = "xorg-consulting-stage-vnet-01"
-    }
+resource "azurerm_virtual_network" "xorg-consulting-stage-vnet" {
+  name = "xorg-consulting-stage-vnet-01"
+  location = azurerm_resource_group.xorg_consulting_stage.location
+  resource_group_name = azurerm_resource_group.xorg_consulting_stage.name
+  address_space = [ "10.0.0.0/16" ]
+  tags = {
+    "Name" = "xorg-consulting-stage-vnet-01",
+    "Environment" = "Stage"
+  }
 }
 
 # Resource 3: Create a Subnet
 
-resource "azurerm_subnet" "xorg-consulting-stage-subnet-01" {
-    name = "xorg-consulting-stage-subnet-01"
+resource "azurerm_subnet" "xorg-consulting-stage-subnet" {
+  name = "xorg-consulting-stage-subnet-01"
+  resource_group_name = azurerm_resource_group.xorg_consulting_stage.name
+  virtual_network_name = azurerm_virtual_network.xorg-consulting-stage-vnet-01.name
+  address_prefixes = [ "10.0.2.0/24" ]
 }
 
 # Resource 4: Create a Public IP address
+
+resource "azurerm_public_ip" "xorg-consulting-stage-publicip" {
+  name = "xorg-consulting-stage-publicip-01"
+  resource_group_name = azurerm_resource_group.xorg_consulting_stage.name
+  location = azurerm_resource_group.xorg_consulting_stage.location
+  allocation_method = "Static"
+  tags = {
+    "Environment" = "Stage"
+  }
+}
+
 # Resource 5: Create a Network Interface
 
+resource "azurerm_network_interface" "xorg-consulting-stage-nic" {
+  
+}
